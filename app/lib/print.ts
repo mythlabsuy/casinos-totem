dde05eb966af95f4fe01f627aba45ee0d7e298d9
Damian Scheck <dscheck@mythologylabs.com.uy>
2024-12-19 13:59:04 -0500

    fix: Do not remove print window automatically

--- app/lib/print.ts
+++ app/lib/print.ts
@@ -12,12 +12,11 @@ export const printPDF = async (blob: Blob | MediaSource) => {
       if(printIframe.contentWindow){
         try {
           printIframe.contentWindow.print()
-          //TODO Ver si esto es necesario, en el modo kiosko aunque esto este comentado la ventana se cierra igual
-          setTimeout(() => {
-            document.body.removeChild(printIframe)
-            URL.revokeObjectURL(url)
-            resolve()
-          }, 2000)
+          // setTimeout(() => {
+          //   document.body.removeChild(printIframe)
+          //   URL.revokeObjectURL(url)
+          //   resolve()
+          // }, 2000)
         } catch (error) {
           reject(new Error('Error al imprimir: ' + error))
         }
