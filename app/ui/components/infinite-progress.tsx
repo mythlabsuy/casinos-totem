75b77e19982ce8f1c0212d230ec381cf8f16543a
Damian Scheck <dscheck@mythologylabs.com.uy>
2025-03-04 13:14:21 -0300

    feat(pc-109): Added loading bar to ticket print screen

--- app/ui/components/infinite-progress.tsx
+++ app/ui/components/infinite-progress.tsx
@@ -8,7 +8,17 @@ export function InfiniteProgressBar({ color } : Props) {
   return (
     <div className="container">
       <div className="progress-bar rounded-b-2xl">
-        <div className={`progress-bar-value ${color}`}></div>
+        <div className={`progress-bar-value ${color}`}/>
+      </div>
+    </div>
+  );
+}
+
+export function InfiniteProgressBarCard({ color } : Props) {
+  return (
+    <div className="w-full relative">
+      <div className="rounded-b-2xl w-full h-[10px] overflow-hidden absolute -top-[10px] z-0">
+        <div className={`progress-bar-value ${color}`}/>
       </div>
     </div>
   );
